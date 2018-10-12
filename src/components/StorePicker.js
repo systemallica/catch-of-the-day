4f2dfbf4e3040604b3279daa5cf89b6ef1770df2
Andrés Reverón Molina <andres@reveronmolina.me>
2018-10-12 13:18:40 +0200

    feat: use helper function to generate random name

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store name" />
+        <input
+          type="text"
+          required
+          //gets deleted when user writes
+          placeholder={getFunName()}
+          //it's a real value that the user would have to replace
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit store -></button>
       </form>
     );
