9f58f391421694a14a2bc0bc5930fd5e9d81a662
Andrés Reverón Molina <andres@reveronmolina.me>
2018-10-13 11:21:51 +0200

    feat: get input value and change router url

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,15 +2,27 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // avoid reloading the page
+    event.preventDefault();
+    // get input value
+    const storeName = this.myInput.value.value;
+    // use the router props to change url and load App component
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
         <input
           type="text"
+          ref={this.myInput}
           required
           //gets deleted when user writes
-          placeholder={getFunName()}
+          placeholder={"Placeholder"}
           //it's a real value that the user would have to replace
           defaultValue={getFunName()}
         />
