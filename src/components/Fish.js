cb4ca7d2bd46fb9c2f1ea2763974806cca55ca2b
Andrés Reverón Molina <andres@reveronmolina.me>
2018-10-14 15:52:27 +0200

    feat: add to order

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -3,6 +3,7 @@ import { formatPrice } from "../helpers";
 class Fish extends React.Component {
   render() {
     const { image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === "available";
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -11,7 +12,12 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add to cart</button>
+        <button
+          onClick={() => this.props.addToOrder(this.props.index)}
+          disabled={!isAvailable}
+        >
+          {isAvailable ? "Add to order" : "Sold out!"}
+        </button>
       </li>
     );
   }
