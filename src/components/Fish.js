47b549de8983bf925e37a857c8402b87997ea6e4
Andrés Reverón Molina <andres@reveronmolina.me>
2018-10-14 17:04:15 +0200

    feat: add Order component

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { formatPrice } from "../helpers";
+
 class Fish extends React.Component {
   render() {
     const { image, name, price, desc, status } = this.props.details;
